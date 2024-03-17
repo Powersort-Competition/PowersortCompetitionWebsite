990ace0d9a2df4ddad1b785b2a7d2b4c22dee299
Shayan Doust <shayan.doust@outlook.com>
2024-03-17 17:16:24 +0000

    Multiple updates - 1. Add vue-cookies as dependency to run during runtime. 2. Add ClientID for Google OAuth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,13 +6,15 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import vue3GoogleLogin from 'vue3-google-login'
+import VueCookies from 'vue-cookies'
 import router from './router'
 
 const app = createApp(App).use(router)
 
+app.use(VueCookies, { expires: '1d' })
 app.use(vue3GoogleLogin,
     {
-        clientId: 'PLACEHOLDER'
+        clientId: '1006096400271-vaib2i6q6dskcfn4q09hv1bigr2im27o.apps.googleusercontent.com'
     })
 
 app.mount('#app')
