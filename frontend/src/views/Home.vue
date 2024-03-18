3b667a000625368477dfb98328d10813b5c5d9bd
Shayan Doust <sgsdoust@liverpool.ac.uk>
2024-03-18 23:21:45 +0000

    Add basis for leaderboard table

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="home">
-    hello
+    <h1>Powersort Competition</h1>
+
+    <br>
+    <div class="leaderboard-table">
+      <table class="table">
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Score</th>
+        </tr>
+        </thead>
+      </table>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const leaderboard_json = ref(null);
+</script>
