4bffba663cea1bfc42ebb30904b0f592a8775bad
Shayan Doust <hello@shayandoust.me>
2024-03-20 21:25:27 +0000

    Move pyodide away from main thread and to its own web worker

--- frontend/src/views/Submission.vue
+++ frontend/src/views/Submission.vue
@@ -1,31 +1,29 @@
 <template>
   <div class = "submission">
-    <h1>Submission</h1>
-
+    <FileDropper />
   </div>
 </template>
 
 <script setup>
+import FileDropper from "@/components/FileDropper.vue";
 import router from '../router/index.js'
 
-// Check if oauth cookie is set. If not, redirect to login.
+/*// Check if oauth cookie is set. If not, redirect to login.
 if ($cookies.get('pscomp_oauth') == null)
 {
 	console.log("Not logged in... routing to login page");
 	router.push({ name: 'login' })
-}
-/**
-import { loadPyodide } from "pyodide";
+}*/
 
-let pyodide = await loadPyodide({
-  indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.25.0/full/'
-});
+import { asyncRun } from '../py_webworker.js'
 
-pyodide.runPythonAsync(`
+const script = `
 from pyodide.ffi import to_js
 from pyodide.http import pyfetch
 response = await pyfetch("https://corsproxy.io/?https%3A%2F%2Fgithub.com%2Fshayandoust%2FPowersortCompetitionWebsite%2Fraw%2Fmain%2Ffrontend%2Fpy_assets%2Fpowersort_timsort.tar.gz") # .zip, .whl, ...
 await response.unpack_archive() # by default, unpacks to the current dir
+
+print("Received and unpacked Python components successfully!")
 
 import Powersort as Powersort
 import Timsort as Timsort
@@ -46,20 +44,26 @@ def compare_sorters(lst, sorters = [Powersort, Timsort]):
     sorters = sorted(sorters, key = lambda sorter: sorter.name())
 
     return to_js([cost(lst, sorter) for sorter in sorters])
-`)
+`
 
-// function processFileSubmission()
-// {
-//   var file = document.getElementById("file").files[0];
-//   var reader = new FileReader();
-//   reader.readAsBinaryString(file);
-//
-//   reader.onload = event =>
-//   {
-//     file_content = event.target.result;
-//     pyodide.runPython(`from js import file_content\nprint(compare_sorters(file_content))`);
-//
-//   }
-// }
-**/
+async function runPyWebWorker()
+{
+  console.log("Pyodide web worker initialising.");
+  try
+  {
+    const { result, error } = await asyncRun(script, []);
+    if (result)
+    {
+      console.log("Pyodide web worker returned: ", result);
+    }
+    else if (error)
+    {
+      console.log("Pyodide web worker failed and returned: ", error);
+    }
+  }
+  catch (e)
+  {
+    console.log(`Error with Pyodide web worker at ${e.filename}, ${e.lineno}, ${e.message}`);
+  }
+}
 </script>
