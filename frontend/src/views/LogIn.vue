07eeb34df145a873e6de10c60a6bcfe3aad35b42
Shayan Doust <hello@shayandoust.me>
2024-03-20 21:36:33 +0000

    Decode user credentials from basic Google data

--- frontend/src/views/LogIn.vue
+++ frontend/src/views/LogIn.vue
@@ -8,11 +8,14 @@
 </template>
 
 <script setup>
-import {GoogleLogin} from "vue3-google-login";
+import { GoogleLogin } from "vue3-google-login";
+import { decodeCredential } from "vue3-google-login";
 
 const callback = (response) => {
-  // This callback will be triggered when the user selects or login to
-  // his Google account from the popup
-  console.log("Handle the response", response)
+  console.log("Google login raw response: ", response)
+
+  const decoded_res = decodeCredential(response.credential)
+
+  console.log("Google login decoded response: ", decoded_res)
 }
 </script>
