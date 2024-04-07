e584b894904501888c493859c6b91cdfa247d865
Shayan Doust <hello@shayandoust.me>
2024-04-07 01:48:50 +0100

    Finally get the home screen leaderboard working!

---
+++ frontend/src/components/Leaderboard.vue
@@ -0,0 +1,38 @@
+<template>
+      <Suspense>
+        <div class="leaderboard-table">
+          <table class="table">
+            <caption>Top 5 submissions</caption>
+            <thead>
+            <tr>
+              <th scope="col">Submission ID</th>
+              <th scope="col">Powersort Comparisons</th>
+              <th scope="col">Timsort Comparisons</th>
+              <th scope="col">Comparison ratio</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for='submission in leaderboardJson'>
+              <td>{{submission.submission_id}}</td>
+              <td>{{submission.powersort_comp}}</td>
+              <td>{{submission.timsort_comp}}</td>
+              <td>{{submission.ratio_comp}}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </Suspense>
+</template>
+
+<script>
+import axios from "axios";
+
+let { data } = await axios.get('https://psortcomp.shayandoust.me/top_5_submissions')
+const leaderboardJson = data;
+
+export default {
+  async setup() {
+    return { leaderboardJson }
+  }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,22 +3,12 @@
     <h1>Powersort Competition</h1>
 
     <br>
-    <div class="leaderboard-table">
-      <table class="table">
-        <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Score</th>
-        </tr>
-        </thead>
-      </table>
-    </div>
+    <Suspense>
+    <Leaderboard />
+    </Suspense>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-
-const leaderboard_json = ref(null);
+import Leaderboard  from "@/components/Leaderboard.vue";
 </script>
