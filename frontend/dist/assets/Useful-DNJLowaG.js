869d15e0c4fac4bad61ed4e02f2fcb66312e40ef
Shayan Doust <hello@shayandoust.me>
2024-04-13 20:54:31 +0100

    General touch-ups

---
+++ frontend/dist/assets/Useful-DNJLowaG.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c}from"./index-C9FwR70A.js";const t={},o={class:"useful"};function _(f,n){return s(),c("div",o," Useful_stuff ")}const a=e(t,[["render",_]]);export{a as default};
--- frontend/src/webworker.js
+++ frontend/src/webworker.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js");
+importScripts("https://cdn.jsdelivr.net/gh/shayandoust/PowersortCompetitionWebsite/dist/pyodide.js");
 
 async function loadPyodideAndPackages() {
     self.pyodide = await loadPyodide();
