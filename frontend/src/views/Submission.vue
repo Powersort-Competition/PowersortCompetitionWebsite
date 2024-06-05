16eb434b7a80909e5f2f1e1b891d3a22ff3e79b1
Shayan Doust <hello@shayandoust.me>
2024-06-05 20:22:14 +0100

    Fix CORS issue as a result of URL migration

--- frontend/src/views/Submission.vue
+++ frontend/src/views/Submission.vue
@@ -106,7 +106,7 @@ const script = `
 from pyodide.ffi import to_js
 from pyodide.http import pyfetch
 
-response = await pyfetch("https://corsproxy.io/?https%3A%2F%2Fgithub.com%2Fshayandoust%2FPowersortCompetitionWebsite%2Fraw%2Fmain%2Ffrontend%2Fpy_assets%2Fpowersort_timsort.tar.gz") # .zip, .whl, ...
+response = await pyfetch("https://raw.githubusercontent.com/Powersort-Competition/PowersortCompetitionWebsite/main/frontend/py_assets/powersort_timsort.tar.gz") # .zip, .whl, ...
 await response.unpack_archive() # by default, unpacks to the current dir
 
 print("Received and unpacked Python components successfully!")
