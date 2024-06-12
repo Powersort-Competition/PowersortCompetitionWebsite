45277dabbe2a8149eae5ea5fe94fb67187c6694d
Shayan Doust <hello@shayandoust.me>
2024-06-12 17:22:25 +0100

    Include prize table to populate later...

---
+++ frontend/src/components/PrizeTable.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <BTable striped hover :items="items"></BTable>
+  </div>
+</template>
+
+<script setup>
+import {BTable} from "bootstrap-vue-next";
+
+const items = [
+      { position: '1st', prize: 'Some prize here...' },
+      { position: '2nd', prize: 'Some prize here...' },
+      { position: '3rd', prize: 'Some prize here...' },
+]
+</script>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -3,6 +3,13 @@
     <h1>Background</h1>
 
     <h1>Competition Details</h1>
+    The competition is split into three discrete classes - <i>flyweight</i>, <i>mediumweight</i> and <i>heavyweight</i>.
+    Each submission is allocated a specific class based on its number of elements. As such, we aim to capture anything
+    from edge cases in small inputs that might cause Timsort to perform greater than Powersort, to larger and more
+    complex inputs.
+
+    <br>
+    <br>
     <BCardGroup deck>
     <BCard bg-variant="light" header="Flyweight" class="text-center">
       <BCardText>Submissions where the number of elements is less than <code>10,000</code>.</BCardText>
@@ -15,11 +22,24 @@
       <BCardText>Submissions where the number of elements is equal to or greater than
       <code>1,000,000</code>.</BCardText>
     </BCard>
-
     </BCardGroup>
+
+    <br>
+    <br>
+    There are prizes to be won for each submission class. You are welcome to submit as many times as you like, and for
+    any of the class!
+
+    <br>
+    <br>
+    <br>
+    <h2>Prizes</h2>
+
+    <PrizeTable/>
   </div>
 </template>
 
 <script setup>
 import {BCard, BCardGroup, BCardText} from "bootstrap-vue-next";
+
+import PrizeTable from "@/components/PrizeTable.vue";
 </script>
