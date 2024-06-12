9191dd5c5a741ca489d24235b8c3d8685e8866ba
Shayan Doust <hello@shayandoust.me>
2024-06-12 23:18:15 +0100

    Style changes, and add more information w.r.t. competition tracks.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,8 +1,65 @@
 <template>
   <div class="useful">
     <h1>Background</h1>
+    <p>
+    Help us as citizen scientist to improve Python's sorting algorithm! Win prizes by solving puzzles and
+    help the climate! Every bit counts.
 
+    <br>
+    <br>
+    Sorting is a core step in many computational tasks, and a faster sorting algorithm can provide faster
+    execution time and reduced energy consumption without changes to application code. Based on our
+    <BLink variant="primary" href="https://www.youtube.com/watch?v=XjOnY-OLAPc" target="_blank" rel="noopener">
+      theoretical computer science research
+    </BLink>,
+    sorting lists in Python is up to [40% faster since Python 3.11](uni News) due to Powersort.
+
+    <br>
+    <br>
+    Help us better understand the algorithm's performance in the wild, and bring its benefits to other communities.
+    And earn some serious prizes along the way!
+
+    <br>
+    <br>
+    We seek your help in 3 tracks:
+    </p>
+
+    <BCardGroup deck>
+      <BCard bg-variant="light" header="Track A" class="text-center">
+        <BCardText>
+          <p>
+            Timsort and Powersort are <i>adaptive</i> sorting algorithms: they are faster if the input has more presorted
+            areas ("runs"). This behavior is based on finding good merge trees (by implicitly solving an optimization
+            problem looking for a nearly [optimal binary search tree](CP ref geeks for geeks or so)!).
+
+            <br>
+            <br>
+            In this track, your goal is to find inputs for which the difference in efficiency between the merge policies
+            of Timsort and Powersort is as big as possible.
+
+            <br>
+            <br>
+            Timsort has some known blind spots where it performs poorly. <b>Can you find more?</b>
+          </p>
+        </BCardText>
+      </BCard>
+      <BCard bg-variant="light" header="Track B" class="text-center">
+        <BCardText>
+          <p>
+            We want to understand sorting performance in your applications.
+          </p>
+        </BCardText>
+      </BCard>
+      <BCard bg-variant="light" header="Track C" class="text-center">
+        <BCardText>
+          <p>
+
+          </p>
+        </BCardText>
+      </BCard>
+    </BCardGroup>
     <h1>Competition Details</h1>
+    <h2>Track A</h2>
     The competition is split into three discrete classes - <i>flyweight</i>, <i>mediumweight</i> and <i>heavyweight</i>.
     Each submission is allocated a specific class based on its number of elements. As such, we aim to capture anything
     from edge cases in small inputs that might cause Timsort to perform greater than Powersort, to larger and more
@@ -29,6 +86,10 @@
     There are prizes to be won for each submission class. You are welcome to submit as many times as you like, and for
     any of the class!
 
+    <h2>Track B</h2>
+
+    <h2>Track C</h2>
+
     <br>
     <br>
     <br>
@@ -39,7 +100,7 @@
 </template>
 
 <script setup>
-import {BCard, BCardGroup, BCardText} from "bootstrap-vue-next";
+import {BCard, BCardGroup, BCardText, BLink} from "bootstrap-vue-next";
 
 import PrizeTable from "@/components/PrizeTable.vue";
 </script>
--- frontend/src/views/Stats.vue
+++ frontend/src/views/Stats.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="body">
+  <div class="stats">
     <h1>Heavyweight</h1>
 
     <suspense>
       <HeavyweightLeaderboard/>
     </suspense>
 
+    <br>
     <h1>Mediumweight</h1>
 
     <suspense>
       <MediumweightLeaderboard/>
     </suspense>
 
+    <br>
     <h1>Flyweight</h1>
 
     <suspense>
