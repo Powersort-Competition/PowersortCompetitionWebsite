94dbcf6a3a8a948b5239f527fcbc659a32fac1ce
Shayan Doust <hello@shayandoust.me>
2024-06-16 19:24:21 +0100

    [Non-functional]: Add alert during submission if server is offline.

---
+++ frontend/src/components/BackendOfflineAlert.vue
@@ -0,0 +1,12 @@
+<template>
+  <BAlert :model-value="true" variant="danger">
+    <p>
+      We are currently experiencing issues with the server. As a result, submissions are temporarily disabled.
+      Please try again later.
+    </p>
+  </BAlert>
+</template>
+
+<script setup>
+import {BAlert} from "bootstrap-vue-next";
+</script>
