712117c65c2c6c6e894b56bda1bff32ca0424c6d
Shayan Doust <hello@shayandoust.me>
2024-06-23 19:25:49 +0100

    Handle saving track B.

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -3,8 +3,8 @@ use std::fs;
 use std::io::Read;
 
 use actix_multipart::form::MultipartForm;
-use actix_web::{get, HttpRequest, HttpResponse, post, web::Json};
 use actix_web::web::Path;
+use actix_web::{get, post, web::Json, HttpRequest, HttpResponse};
 use diesel::prelude::*;
 use futures::executor::block_on;
 
@@ -13,13 +13,12 @@ use crate::database::init_db;
 use crate::mailer;
 use crate::models::{FileDownload, NewSubmission, NewUser, Submission, SubmissionHash, User};
 use crate::schema::tracka_submission_hashes::dsl::tracka_submission_hashes;
-use crate::schema::tracka_submissions::{perc_diff, submission_id, submission_size};
 use crate::schema::tracka_submissions::dsl::tracka_submissions;
+use crate::schema::tracka_submissions::{perc_diff, submission_id, submission_size};
 use crate::schema::users::dsl::*;
 
 #[get("/ping")]
-pub async fn ping() -> HttpResponse
-{
+pub async fn ping() -> HttpResponse {
     HttpResponse::Ok().json("pong".to_string())
 }
 
@@ -27,20 +26,27 @@ pub async fn ping() -> HttpResponse
 Internal function to check if user exists by probing database with email.
  */
 #[allow(unused_parens)]
-fn check_usr_exists(mut conn: PgConnection, email_addr: String) -> bool
-{
-    //let res = users.select(User::as_select()).load(&mut conn);
-    //println!("{:?}", res.unwrap().get(1).unwrap().email);
+fn check_usr_exists(mut conn: PgConnection, email_addr: String) -> bool {
     let res = users.filter(email.eq(email_addr)).load::<User>(&mut conn);
-    let res_size = res.expect("Error checking if user exists in database!").len();
+    let res_size = res
+        .expect("Error checking if user exists in database!")
+        .len();
 
-    if (res_size == 1) { return true; } else { return false; }
+    if (res_size == 1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-fn dispatch_mail_receipt(email_addr: String, submission_hash_str: String, submission: NewSubmission)
-{
+fn dispatch_mail_receipt(
+    email_addr: String,
+    submission_hash_str: String,
+    submission: NewSubmission,
+) {
     println!("Hash submission string: {}", submission_hash_str);
-    let body = format!("Hello! Your submission has been recorded successfully. \
+    let body = format!(
+        "Hello! Your submission has been recorded successfully. \
                         \n\n<br> <br> Powersort Comparison: {} \
                         \n<br> Timsort Comparison: {} \
                         \n<br> Difference in merge costs: {} \
@@ -48,34 +54,42 @@ fn dispatch_mail_receipt(email_addr: String, submission_hash_str: String, submis
                         \n<br> Timsort Merge Cost: {} \
                         \n<br> Submission Size: {} \
                          \n\n<br> <br> <br> <br> <br> {} ",
-                       submission.powersort_comp,
-                       submission.timsort_comp,
-                       submission.perc_diff,
-                       submission.powersort_merge_cost,
-                       submission.timsort_merge_cost,
-                       submission.submission_size,
-                       submission_hash_str);
+        submission.powersort_comp,
+        submission.timsort_comp,
+        submission.perc_diff,
+        submission.powersort_merge_cost,
+        submission.timsort_merge_cost,
+        submission.submission_size,
+        submission_hash_str
+    );
 
     mailer::send_email(body, email_addr);
 }
 
-fn get_email_from_user_id(usr_id: i32) -> String
-{
-    let res = users.filter(user_id.eq(usr_id)).load::<User>(&mut init_db());
+fn get_email_from_user_id(usr_id: i32) -> String {
+    let res = users
+        .filter(user_id.eq(usr_id))
+        .load::<User>(&mut init_db());
 
-    return res.expect("Error loading user email!").get(0).unwrap().email.clone();
+    return res
+        .expect("Error loading user email!")
+        .get(0)
+        .unwrap()
+        .email
+        .clone();
 }
 
 // Top 5 GLOBAL submissions irrespective of upload size.
 #[get("/top_5_submissions")]
-pub async fn top_5_submissions() -> HttpResponse
-{
-    let res = tracka_submissions.order(perc_diff.desc()).limit(5).load::<Submission>(&mut init_db());
+pub async fn top_5_submissions() -> HttpResponse {
+    let res = tracka_submissions
+        .order(perc_diff.desc())
+        .limit(5)
+        .load::<Submission>(&mut init_db());
 
     let mut top_5 = Vec::new();
 
-    for submission in res.expect("Error loading top 5 submissions!")
-    {
+    for submission in res.expect("Error loading top 5 submissions!") {
         top_5.push(submission);
     }
 
@@ -84,28 +98,30 @@ pub async fn top_5_submissions() -> HttpResponse
 
 // class = flyweight OR mediumweight OR heavyweight.
 #[get("/weightclass_leading_submissions/{class}")]
-pub async fn weightclass_leading_submissions(class: Path<String>) -> HttpResponse
-{
+pub async fn weightclass_leading_submissions(class: Path<String>) -> HttpResponse {
     let _class = class.into_inner();
     println!("Getting top 5 submissions for weight class: {}", _class);
 
     let res;
 
-    if (_class == "flyweight")
-    {
+    if (_class == "flyweight") {
         res = tracka_submissions
             .filter(submission_size.lt(i32::pow(10, 4)))
             .order(perc_diff.desc())
             .limit(5)
             .load::<Submission>(&mut init_db());
-    } else if (_class == "mediumweight")
-    {
+    } else if (_class == "mediumweight") {
         res = tracka_submissions
-            .filter(submission_size.ge(i32::pow(10, 4)).and(submission_size.lt(i32::pow(10, 6))))
+            .filter(
+                submission_size
+                    .ge(i32::pow(10, 4))
+                    .and(submission_size.lt(i32::pow(10, 6))),
+            )
             .order(perc_diff.desc())
             .limit(5)
             .load::<Submission>(&mut init_db());
-    } else // heavyweight
+    } else
+    // heavyweight
     {
         res = tracka_submissions
             .filter(submission_size.ge(i32::pow(10, 6)))
@@ -116,8 +132,7 @@ pub async fn weightclass_leading_submissions(class: Path<String>) -> HttpRespons
 
     let mut top_5 = Vec::new();
 
-    for submission in res.expect("Error loading top 5 flyweight submissions!")
-    {
+    for submission in res.expect("Error loading top 5 flyweight submissions!") {
         top_5.push(submission);
     }
 
@@ -126,44 +141,42 @@ pub async fn weightclass_leading_submissions(class: Path<String>) -> HttpRespons
 
 #[allow(unused_parens)]
 #[post("/logged_in")]
-pub async fn login_probe(usr_details: Json<NewUser>) -> HttpResponse
-{
+pub async fn login_probe(usr_details: Json<NewUser>) -> HttpResponse {
     // Check if user already exists in the database via their email address.
     let usr_exists = check_usr_exists(init_db(), usr_details.email.clone());
 
-    if (!usr_exists)
-    {
+    if (!usr_exists) {
         println!("Creating new user in database.");
         // Create a new user in the database.
-        let _new_usr = NewUser
-        {
+        let _new_usr = NewUser {
             first_name: usr_details.first_name.clone(),
             last_name: usr_details.last_name.clone(),
             email: usr_details.email.clone(),
         };
 
-        let _ = diesel::insert_into(users).values(&_new_usr).execute(&mut init_db());
+        let _ = diesel::insert_into(users)
+            .values(&_new_usr)
+            .execute(&mut init_db());
     }
 
     HttpResponse::Ok().json("Success".to_string())
 }
 
 #[post("/my_user_id")]
-pub async fn my_user_id(usr_details: Json<User>) -> HttpResponse
-{
+pub async fn my_user_id(usr_details: Json<User>) -> HttpResponse {
     // We are only concerned with user email, as that is unique.
-    let res = users.filter(email.eq(usr_details.email.clone())).load::<User>(&mut init_db());
+    let res = users
+        .filter(email.eq(usr_details.email.clone()))
+        .load::<User>(&mut init_db());
 
     HttpResponse::Ok().json(res.expect("Error loading user ID!").get(0).unwrap().user_id)
 }
 
 #[post("/new_submission")]
-pub async fn new_submission(submission: Json<NewSubmission>) -> HttpResponse
-{
+pub async fn new_submission(submission: Json<NewSubmission>) -> HttpResponse {
     println!("Adding new user submission to database.");
 
-    let _new_submission = NewSubmission
-    {
+    let _new_submission = NewSubmission {
         user_id: submission.user_id,
         powersort_comp: submission.powersort_comp.clone(),
         timsort_comp: submission.timsort_comp.clone(),
@@ -172,21 +185,23 @@ pub async fn new_submission(submission: Json<NewSubmission>) -> HttpResponse
         timsort_merge_cost: submission.timsort_merge_cost.clone(),
         submission_size: submission.submission_size.clone(),
     };
-    let s_id = diesel::insert_into(tracka_submissions).values(&_new_submission)
+    let s_id = diesel::insert_into(tracka_submissions)
+        .values(&_new_submission)
         .returning(submission_id)
         .get_result::<i32>(&mut init_db())
         .unwrap();
 
     // Hash submission ID, and record it to the database. Then, send it via email.
     let submission_hash_str = block_on(hash_submission(s_id));
-    let _submission_hash = SubmissionHash
-    {
+    let _submission_hash = SubmissionHash {
         submission_id: s_id,
         hash: submission_hash_str.clone(),
     };
 
-    let _ = diesel::insert_into(tracka_submission_hashes).values(&_submission_hash)
-        .execute(&mut init_db()).unwrap();
+    let _ = diesel::insert_into(tracka_submission_hashes)
+        .values(&_submission_hash)
+        .execute(&mut init_db())
+        .unwrap();
 
     // Send email receipt to user.
     let email_addr = get_email_from_user_id(_new_submission.user_id);
@@ -196,9 +211,10 @@ pub async fn new_submission(submission: Json<NewSubmission>) -> HttpResponse
 }
 
 #[post("/submission_input_save")]
-pub async fn submission_input_save(req: HttpRequest,
-                                   MultipartForm(mut form): MultipartForm<FileDownload>) -> HttpResponse
-{
+pub async fn submission_input_save(
+    req: HttpRequest,
+    MultipartForm(mut form): MultipartForm<FileDownload>,
+) -> HttpResponse {
     println!("Saving user submission input.");
 
     let headers = req.headers();
@@ -206,19 +222,45 @@ pub async fn submission_input_save(req: HttpRequest,
 
     let file_name = headers.get("file-name").unwrap().to_str().unwrap();
     let track = headers.get("track").unwrap().to_str().unwrap();
+    println!("Track: {}", track);
 
     let file_path;
-    if (track == "A") // Track A
+    if (track == "A")
+    // Track A
     {
-        file_path = format!("{}/submissions/TrackA/{}", env::var("EGRESS_DIR").unwrap(),
-                            file_name);
-    } else // Track B
+        file_path = format!(
+            "{}/submissions/TrackA/{}",
+            env::var("EGRESS_DIR").unwrap(),
+            file_name
+        );
+        
+        form.file[0]
+            .file
+            .read_to_string(&mut contents)
+            .expect("Error reading file contents!");
+    } else
+    // Track B
     {
-        file_path = format!("{}/submissions/TrackB/{}", env::var("EGRESS_DIR").unwrap(),
-                            file_name);
-    }
+        // Special handling is required for Track B submissions, as we have descriptions too.
+        let mut input_data = String::new();
+        let mut description = String::new();
+        
+        file_path = format!(
+            "{}/submissions/TrackB/{}",
+            env::var("EGRESS_DIR").unwrap(),
+            file_name
+        );
 
-    form.file[0].file.read_to_string(&mut contents).expect("Error reading file contents!");
+        form.file[0]
+            .file
+            .read_to_string(&mut input_data)
+            .expect("Error reading file contents!");
+        
+        description = headers.get("description").unwrap().to_str().unwrap().parse().unwrap();
+        
+        // Concatenate the input data and description into contents variable.
+        contents = format!("{}\n\n{}", input_data, description);
+    }
 
     // Save the file contents to the file system.
     fs::write(&file_path, &contents).expect("Unable to write file to disk!");
@@ -227,8 +269,7 @@ pub async fn submission_input_save(req: HttpRequest,
 }
 
 #[post("/serverside_calc")]
-pub async fn serverside_calc(input: Json<String>) -> HttpResponse
-{
+pub async fn serverside_calc(input: Json<String>) -> HttpResponse {
     println!("Performing server-side calculation.");
 
     HttpResponse::Ok().json("Success".to_string())
