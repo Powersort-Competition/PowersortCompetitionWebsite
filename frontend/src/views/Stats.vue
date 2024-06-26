1b02aebafeb6d9bbe58eb610524080394fee2335
Shayan Doust <hello@shayandoust.me>
2024-06-26 21:52:58 +0100

    Add radio buttons to sort stats page by one of either performance 'metric'.

--- frontend/src/views/Stats.vue
+++ frontend/src/views/Stats.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="container mt-4 main">
+  <div class="container mt-4">
     <h1>Leaderboards</h1>
+    <BFormRadioGroup
+      v-model="selectedMetricType"
+      :options="metricTypes"
+      class="mb-10 text-right"
+      value-field="item"
+      text-field="name"
+    />
     <h2>Heavyweight</h2>
 
     <suspense>
@@ -29,6 +36,15 @@
 import HeavyweightLeaderboard from "@/components/HeavyweightLeaderboard.vue";
 import MediumweightLeaderboard from "@/components/MediumweightLeaderboard.vue";
 import FlyweightLeaderboard from "@/components/FlyweightLeaderboard.vue";
+
+import {BFormRadioGroup} from "bootstrap-vue-next";
+import {ref} from "vue";
+
+const selectedMetricType = ref('perc_diff');
+const metricTypes = [
+  {item: 'perc_diff', name: 'Merge Cost Difference (%)'},
+  {item: 'comp_diff', name: 'Comparison Count Difference'}
+]
 </script>
 
 <style scoped></style>
