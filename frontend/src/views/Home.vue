c138ddb2747a5ddcdd6cf5556d6cda81c6ad8bc8
Sebastian Wild <wild@liverpool.ac.uk>
2024-06-26 18:57:59 +0100

    more text on tracks

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -37,45 +37,72 @@
     </p>
     <ul>
       <li>
-        <font-awesome-icon icon="puzzle-piece" fixed-width/> Track A: Exploratory data puzzle
+        <font-awesome-icon icon="puzzle-piece" fixed-width/>
+        Track A: Exploratory data puzzle
       </li>
       <li>
-        <font-awesome-icon icon="database" fixed-width/> Track B: Contribute to a sorting benchmark
+        <font-awesome-icon icon="database" fixed-width/>
+        Track B: Contribute to a sorting benchmark
       </li>
       <li>
-        <font-awesome-icon icon="code-merge" fixed-width /> Track C: Implementation sprint for Powersort in software frameworks
+        <font-awesome-icon icon="code-merge" fixed-width/>
+        Track C: Implementation sprint for Powersort in software frameworks
       </li>
     </ul>
 
-
-
-    <h4>Track A: Sort this, Timsort!</h4>
+    <p>
+      Track B requires no algorithmic knowledge; you just run your favorite Python project
+      on our virtual machine.
+      Track A requires some tinkering with the algorithms, but we have a demo notebook to
+      get you started.
+      Track C is for developers who want to get their hands dirty with production code.
+    </p>
+    <h4>
+      <font-awesome-icon icon="puzzle-piece"/>
+      Track A: Sort this, Timsort!
+    </h4>
     <p>
       In this <strong>exploratory data puzzle</strong>, your goal is to find lists for
       Timsort and Powersort where they differ the most.
-
     </p>
-    <p>“You can't sort this list (with Timsort as fast as you should)”
+    <p>
+      <!--      “You can't sort this list (with Timsort as fast as you should)”-->
 
-      Timsort and Powersort are *adaptive* sorting algorithms: they are faster if the
-      input
-      has more presorted areas ("runs"). This behavior is based on finding good merge
-      trees
-      (by implicitly solving an optimization problem looking for a nearly [optimal binary
-      search tree](CP ref geeks for geeks or so)!)
+      Timsort and Powersort are <a href="https://en.wikipedia.org/wiki/Adaptive_sort"><em>adaptive</em>
+      sorting algorithms</a>:
+      they are faster if the input has more presorted areas (“runs”).
+      This behavior is based on finding good merge trees
+      (by implicitly solving an optimization problem looking for a nearly
+      <a href="https://en.wikipedia.org/wiki/Optimal_binary_search_tree">optimal binary
+        search tree</a>!)
 
-      In this track, your goal is to find inputs for which the difference in efficiency
-      between the merge policies of Timsort and Powersort is as big as possible.
-      Timsort has some known blind spots where it performs poorly. Can you find more?
+      In Track A, your goal is to find inputs for which the <strong>difference in
+      efficiency</strong>
+      between the merge policies of <strong>Timsort and Powersort</strong> is as big as
+      possible.
     </p>
-    <h4>Track B: Sorting-in-the-Wild Benchmark</h4>
+    <p>
+      Timsort has some known blind spots where it performs poorly
+      (see <!--$$\mathcal R_{\text{tim}}$$--> Theorem 3 of <a
+        href="https://arxiv.org/pdf/1801.04641">[BK2018]</a>.
+    </p>
+    <p>
+      <em>Can you find more?</em>
+    </p>
+    <h4>
+      <font-awesome-icon icon="database"/>
+      Track B: Sorting-in-the-Wild Benchmark
+    </h4>
     <p>
       Crowdsourced benchmark
 
       We want to understand sorting performance in your applications.
     </p>
 
-    <h4>Track C: Commit (to) the future of sorting!</h4>
+    <h4>
+      <font-awesome-icon icon="code-merge"/>
+      Track C: Commit (to) the future of sorting!
+    </h4>
     <p>Implementation sprint</p>
 
 
@@ -96,4 +123,5 @@
 import Leaderboard from "@/components/Leaderboard.vue";
 import {BLink} from "bootstrap-vue-next";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
 </script>
