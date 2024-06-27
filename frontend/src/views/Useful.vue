f646d2dd1dc2c6469e70edfd78c9629b2bb35f62
Sebastian Wild <wild@liverpool.ac.uk>
2024-06-27 22:52:45 +0100

    nicer formatting

--- frontend/src/views/Useful.vue
+++ frontend/src/views/Useful.vue
@@ -1,41 +1,36 @@
 <template>
   <div class="container mt-4 main">
-    <h2>"What file formats can I use for the submission?"</h2>
+    <h4>
+      <font-awesome-icon icon="question-circle"/>
+      What file formats can I use for the submission?
+    </h4>
     <p>
       Currently, the only accepted file format is a plain text file.
       The contents of the files should be a list of comma separated elements,
       with the list enclosed in square brackets. For example, the following
       are valid contents of a file:
-
-      <br/>
       <br/>
       <code> [1, 'b', -1025, 3, 'a', 5] </code>
     </p>
 
-    Notice that valid elements are those that have intrinsic lexicographic ordering.
+    <p>Notice that valid elements are those that have intrinsic lexicographic
+      ordering.</p>
 
-    <br/>
-    <br/>
-    <h2>"What is the maximum file size allowed?"</h2>
+    <h4>
+      <font-awesome-icon icon="question-circle"/>
+      What is the maximum file size allowed?
+    </h4>
     <p>
-      There is no upper limit on the file size. However, as of right now, the number
-      of elements should not exceed <code>TODO</code> elements, due to limitations
-      of in-browser resource allocation towards computing the costs of Timsort and
-      Powersort.
-
-      <br/>
-      <br/>
-      This will, however, change in the upcoming days, with an upper limit of <code>15^6</code>
-      elements being feasible.
+      There is no hard limit on the file size.
+      For short lists, the costs of Timsort and Powersort are computed in the browser
+      using pyodide, a Python interpreter compiled to WebAssembly.
+      For these, results are available immediately.
+      Due to limitations of in-browser resource allocation, large inputs are
+      automatically submitted to our server for processing and results will be sent
+      to you via email.
     </p>
-
-    <br/>
-    <br/>
-    <h2>"How can I start? I need some inspiration!"</h2>
-    <p>
-
-    </p>
-
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+</script>
