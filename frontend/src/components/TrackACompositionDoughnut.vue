b31c5a2a4685df97037aac990fb31b64217fedc9
Shayan Doust <hello@shayandoust.me>
2024-06-28 21:54:05 +0100

    Initial chart commit
    Future charts, especially those closely relevant, can be added side by side.

---
+++ frontend/src/components/TrackACompositionDoughnut.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="dnut">
+    <Doughnut :data="data" :options="options"/>
+  </div>
+</template>
+
+<script setup>
+import {ArcElement, Tooltip, Legend, Chart as chartJS, Title} from 'chart.js';
+import {Doughnut} from 'vue-chartjs';
+import colors from 'vuetify/util/colors';
+
+chartJS.register(ArcElement, Tooltip, Legend, Title);
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Number of track A submissions by weight class',
+    }
+  }
+}
+
+const data = {
+  labels: ['Flyweight', 'Mediumweight', 'Heavyweight'],
+  datasets: [
+    {
+      backgroundColor: [colors.green.darken1, colors.blue.darken1, colors.red.darken1],
+      data: [30, 20, 100]
+    }
+  ],
+  hoverOffset: 4
+}
+</script>
+
+<style scoped>
+.dnut {
+  position: relative;
+  height: 40vh;
+  width: 80vw;
+}
+</style>
